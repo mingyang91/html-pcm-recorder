024f0e342ad13a539807c63b84c7ca2c815f48b6
明扬 <my@famer.me>
2024-04-24 16:14:07 +0800

    Refactor audio recording logic and add comments for clarity

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -5,49 +5,49 @@ import { audioStream, toWave } from './recorder'
 type RecordState = 'Stop' | 'Recording'
 
 function App() {
-	const [state, setState] = useState<RecordState>('Stop')
-	const [list, setList] = useState<string[]>([])
-	useEffect(() => {
-		if (state === 'Recording') {
-			const stream = audioStream()
-			toWave(stream, {
-				sampleRate: 16000,
-				channels: 1,
-				bitDepth: 16
-			}).then(buffers => {
-				const blob = new Blob(buffers, { type: 'audio/wav' })
-				const url = URL.createObjectURL(blob)
-				setList([url, ...list]) 
-			})
-			return stream.complete.bind(stream)
-		}
-		return 
-	}, [state])
+  const [state, setState] = useState<RecordState>('Stop')
+  const [list, setList] = useState<string[]>([])
+  useEffect(() => {
+    if (state === 'Recording') {
+      const stream = audioStream()
+      toWave(stream, {
+        sampleRate: 16000,
+        channels: 1,
+        bitDepth: 16
+      }).then(buffers => {
+        const blob = new Blob(buffers, { type: 'audio/wav' })
+        const url = URL.createObjectURL(blob)
+        setList([url, ...list])
+      })
+      return stream.complete.bind(stream)
+    }
+    return
+  }, [state])
 
-	function toggle() {
-		if (state === 'Stop') setState('Recording')
-		else setState('Stop')
-	}
+  function toggle() {
+    if (state === 'Stop') setState('Recording')
+    else setState('Stop')
+  }
 
-	function buttonText() {
-		if (state === 'Stop') return 'Start'
-		else return 'Stop'
-	}
+  function buttonText() {
+    if (state === 'Stop') return 'Start'
+    else return 'Stop'
+  }
 
-	return <div>
-		Hello, World!
-		<button onClick={toggle}>
-			{buttonText()}
-		</button>
-		<ul>
-			{list.map((item, index) => {
-				return <li key={index}>
-					<audio controls src={item} />
-					<a href={item} download={`recording-${index}.wav`}>Save</a>
-				</li>
-			})}
-		</ul>
-	</div>
+  return <div>
+    Hello, World!
+    <button onClick={toggle}>
+      {buttonText()}
+    </button>
+    <ul>
+      {list.map((item, index) => {
+        return <li key={index}>
+          <audio controls src={item} />
+          <a href={item} download={`recording-${index}.wav`}>Save</a>
+        </li>
+      })}
+    </ul>
+  </div>
 }
 
 const root = createRoot(document.getElementById('app')!);
--- src/worker/recorderWorkletProcessor.ts
+++ src/worker/recorderWorkletProcessor.ts
@@ -1,7 +1,7 @@
 /**
  An in-place replacement for ScriptProcessorNode using AudioWorklet
  */
- class RecorderProcessor extends AudioWorkletProcessor {
+class RecorderProcessor extends AudioWorkletProcessor {
   // 0. Determine the buffer size (this is the same as the 1st argument of ScriptProcessor)
   bufferSize = 2048
   // 1. Track the current buffer fill level
@@ -11,20 +11,20 @@
   _buffer = new Float32Array(this.bufferSize)
 
   constructor() {
-      super()
-      this.initBuffer()
+    super()
+    this.initBuffer()
   }
 
   initBuffer() {
-      this._bytesWritten = 0
+    this._bytesWritten = 0
   }
 
   isBufferEmpty() {
-      return this._bytesWritten === 0
+    return this._bytesWritten === 0
   }
 
   isBufferFull() {
-      return this._bytesWritten === this.bufferSize
+    return this._bytesWritten === this.bufferSize
   }
 
   /**
@@ -32,10 +32,10 @@
    * @returns {boolean}
    */
   process(inputs: Float32Array[][]) {
-      // Grabbing the 1st channel similar to ScriptProcessorNode
-      this.append(inputs[0][0])
+    // Grabbing the 1st channel similar to ScriptProcessorNode
+    this.append(inputs[0][0])
 
-      return true
+    return true
   }
 
   /**
@@ -43,53 +43,53 @@
    * @param {Float32Array} channelData
    */
   append(channelData: Float32Array) {
-      if (this.isBufferFull()) {
-          this.flush()
-      }
+    if (this.isBufferFull()) {
+      this.flush()
+    }
 
-      if (!channelData) return
+    if (!channelData) return
 
-      for (let i = 0; i < channelData.length; i++) {
-          this._buffer[this._bytesWritten++] = channelData[i]
-      }
+    for (let i = 0; i < channelData.length; i++) {
+      this._buffer[this._bytesWritten++] = channelData[i]
+    }
   }
 
   flush() {
-      // trim the buffer if ended prematurely
-      const buffer = this._bytesWritten < this.bufferSize
-          ? this._buffer.slice(0, this._bytesWritten)
-          : this._buffer
-      const result = this.downsampleBuffer(buffer, 44100, 16000);
-      this.port.postMessage(result)
-      this.initBuffer()
+    // trim the buffer if ended prematurely
+    const buffer = this._bytesWritten < this.bufferSize
+      ? this._buffer.slice(0, this._bytesWritten)
+      : this._buffer
+    const result = this.downsampleBuffer(buffer, 44100, 16000);
+    this.port.postMessage(result)
+    this.initBuffer()
   }
 
-  downsampleBuffer (buffer: Float32Array, sampleRate: number, outSampleRate: number) {
-      if (outSampleRate == sampleRate) {
-          return buffer;
+  downsampleBuffer(buffer: Float32Array, sampleRate: number, outSampleRate: number) {
+    if (outSampleRate == sampleRate) {
+      return buffer;
+    }
+    if (outSampleRate > sampleRate) {
+      throw 'downsampling rate show be smaller than original sample rate';
+    }
+    var sampleRateRatio = sampleRate / outSampleRate;
+    var newLength = Math.round(buffer.length / sampleRateRatio);
+    var result = new Int16Array(newLength);
+    var offsetResult = 0;
+    var offsetBuffer = 0;
+    while (offsetResult < result.length) {
+      var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+      var accum = 0,
+        count = 0;
+      for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+        accum += buffer[i];
+        count++;
       }
-      if (outSampleRate > sampleRate) {
-          throw 'downsampling rate show be smaller than original sample rate';
-      }
-      var sampleRateRatio = sampleRate / outSampleRate;
-      var newLength = Math.round(buffer.length / sampleRateRatio);
-      var result = new Int16Array(newLength);
-      var offsetResult = 0;
-      var offsetBuffer = 0;
-      while (offsetResult < result.length) {
-          var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
-          var accum = 0,
-              count = 0;
-          for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
-              accum += buffer[i];
-              count++;
-          }
 
-          result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
-          offsetResult++;
-          offsetBuffer = nextOffsetBuffer;
-      }
-      return result.buffer;
+      result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
+      offsetResult++;
+      offsetBuffer = nextOffsetBuffer;
+    }
+    return result.buffer;
   };
 
 }
